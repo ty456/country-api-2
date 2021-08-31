aa066b406bcb452e4bc53b744cbfc788da62f8ec
shovan <rchpurnell.20@gmail.com>
2021-08-31 15:51:38 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const loadCountries = () => {
+    fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(data => displayCountries(data));
+}
+loadCountries();
+
+const displayCountries = countries => {
+    const countriesDiv = document.getElementById('countries');
+    countries.forEach(country => {
+
+        const div = document.createElement('div');
+        div.classList.add('country');
+        div.innerHTML = `<h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name}')">Details</button>`
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
+        // div.appendChild(h3)
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // countriesDiv.appendChild(p)
+        // div.appendChild(p);
+        countriesDiv.appendChild(div);
+    });
+
+}
+const loadCountryByName = name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDeatil(data[0]));
+}
+const displayCountryDeatil = country => {
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `<h5>country name:${country.name}</h5>
+    <p>country population:${country.population}</p>
+    <img width="150px" src="${country.flag}">
+    `
+
+}
